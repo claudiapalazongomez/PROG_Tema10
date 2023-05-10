f6c8f9c0247aee0cc910fa0bff607ebf746b75f2
Claudia Palazón Gómez <98claudia@gmail.com>
2023-05-10 09:48:45 +0200

    New exercises

---
+++ src/es/studium/Ejemplos/Ejemplo1iText.java
@@ -0,0 +1,41 @@
+package es.studium.Ejemplos;
+
+import java.io.IOException;
+import java.awt.Desktop;
+import java.io.File;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
+public class Ejemplo1iText
+{
+	public static final String DEST = "HolaMundo.pdf"; // nombre del fichero que se va a generar
+
+	public static void main(String[] args)
+	{
+		new Ejemplo1iText(DEST);
+	}
+
+	public Ejemplo1iText(String dest)
+	{
+		try
+		{
+			// Initialize PDF writer
+			PdfWriter writer = new PdfWriter(dest);
+			// Initialize PDF document
+			PdfDocument pdf = new PdfDocument(writer); // puede tener más de una página
+			// Initialize document
+			Document document = new Document(pdf);
+			// Add paragraph to the document
+			document.add(new Paragraph("Hello World!"));
+			// Close document
+			document.close();
+			// Open the new PDF document just created
+			Desktop.getDesktop().open(new File(DEST));
+		} catch (IOException ioe)
+		{
+		}
+	}
+
+}
